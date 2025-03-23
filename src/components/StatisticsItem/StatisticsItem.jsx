afe585a6e58f2bac11b2da7562fbca71f997b2b4
PushEax <pusheax.vk@gmail.com>
2025-03-23 15:32:48 +0200

    complete statistic

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,5 +1,13 @@
-const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import style from './StatisticsItem.module.css';
+
+const StatisticsItem = ({ icon: Icon, title, total }) => {
+  return (
+    <>
+      <Icon size="32px" />
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </>
+  );
 };
 
 export default StatisticsItem;
