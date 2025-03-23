dc483b2291801d4040c47d819c1d8672e053320b
PushEax <pusheax.vk@gmail.com>
2025-03-23 15:32:56 +0200

    completet frobs

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Heading from './components/Heading/Heading';
 import Section from './components/Section/Section';
 import Statistics from './components/Statistics/Statistics';
 import article from './data/article.json';
+import stats from './data/stats.json';
+import forbes from './data/forbes.json';
 
 export const App = () => {
   return (
@@ -15,10 +17,10 @@ export const App = () => {
         <BlogCard {...article} />
 
         <Heading title="Task 2 Statistics" top bottom />
-        <Statistics title="Main Statistics" />
+        <Statistics title="Main Statistics" stats={stats} />
 
         <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList list={forbes} />
 
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
